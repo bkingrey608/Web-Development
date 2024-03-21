750cb09bc5cb6e084386bb25f474d2f71e11a514
bkingrey608 <157029591+bkingrey608@users.noreply.github.com>
2024-03-21 14:49:24 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+
+// Get user input
 const form = document.getElementById('form');
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
 const email = document.getElementById('email');
 
+// add event listener to check for error before submitting
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     validateInputs();
 });
 
+// Add error class / remove success class to field to change style color, display error message
 const setError = (element, message) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
@@ -18,6 +22,7 @@ const setError = (element, message) => {
     inputControl.classList.remove('success')
 }
 
+// Add success class / remove error class to field to change style color
 const setSuccess = element => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
@@ -33,26 +38,27 @@ const isValidEmail = email => {
     return re.test(String(email).toLowerCase());
 }
 
+// Trim blank space from user iput
 const validateInputs = () => {
     const firstNameValue = firstName.value.trim();
     const lastNameValue = lastName.value.trim();
     const emailValue = email.value.trim();
     
-
-    if(firstNameValue === '') {
+// Check for empty or null fields, set succcess/error trigger
+    if(firstNameValue === '' || firstNameValue === null) {
         setError(firstName, 'First name is required');
     } else {
         setSuccess(firstName);
     }
 
     
-    if(lastNameValue === '') {
+    if(lastNameValue === '' || lastNameValue === null) {
         setError(lastName, 'Last name is required');
     } else {
         setSuccess(lastName);
     }
 
-    if(emailValue === '') {
+    if(emailValue === '' || null) {
         setError(email, 'Email is required');
     } else if (!isValidEmail(emailValue)) {
         setError(email, 'Provide a valid email address');
